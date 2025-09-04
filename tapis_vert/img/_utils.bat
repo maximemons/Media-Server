8b76d07c6f4d1509e00fc4f0a8a5a4ae9fb772bf
Maxime Mons <pro.maxime.mons@gmail.com>
2025-09-04 14:41:29 +0200

    [del]

--- tapis_vert/img/_utils.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-REM Renomme tous les fichiers du dossier :
-REM - minuscules
-REM - espaces -> _
-REM - supprime :
-powershell -NoProfile -ExecutionPolicy Bypass -Command ^
-  "Get-ChildItem -LiteralPath . -File | ForEach-Object { $old=$_.Name; $new=$old.ToLower().Replace(' ','_').Replace(':',''); if($new -ne $old){ Rename-Item -LiteralPath $_.FullName -NewName $new } }"
-echo Fini !
-pause
